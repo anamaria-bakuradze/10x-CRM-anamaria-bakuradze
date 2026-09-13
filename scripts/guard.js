07966ec20a219ac6a73aa02828530014b3922aa0
whiteoctopusfrommc <anana1010@proton.me>
2026-09-13 15:00:10 +0400

    there was a mistake in the previous commit

--- scripts/guard.js
+++ scripts/guard.js
@@ -2,11 +2,11 @@ const page = window.location.pathname.replace(/\/+$/, '');
 const path = page.split('/').pop().replace(/\.html$/, '');
 
 if (localStorage.getItem("crm_session")) {
-  if (['index', 'signup'].includes(page)) {
+  if (['index', 'signup'].includes(path)) {
     // alert("here");
     window.location.replace("../general/dashboard.html");
   }
-} else if (!['index', 'signup'].includes(page)) {
+} else if (!['index', 'signup'].includes(path)) {
   // alert("here");
   window.location.replace("../index.html");
 }
